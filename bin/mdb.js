1a2070e77cebdfac91c5a227d41081e5b9ea75eb
rishichawda <chawda.rishii@gmail.com>
2019-02-23 17:33:21 +0530

    fix(bin/mdb): minor fix on generateHook

--- bin/mdb.js
+++ bin/mdb.js
@@ -6,64 +6,80 @@ const fs = require("fs");
 const log = console.log;
 
 const generateHook = () => {
-  glob(path.join(process.cwd(), ".git/hooks/post-checkout"), function(
-    err,
-    files
-  ) {
-    if (err) {
-      console.error(
-        `Unable to verify as a git repository. Try running \`git init\` and initialising ${__dirname} as a git repository.`
+  glob(".git", function(err, files) {
+    if (err || !files.length) {
+      log(
+        chalk`{bold.redBright Error: }{yellowBright ${err ||
+          `Unable to verify as a git repository. Try running \`git init\` and initialising`}} ${chalk.black.bgYellowBright(
+          process.cwd()
+        )} {yellowBright ${`as a git repository.`}}`
       );
-      throw err;
+      return;
     }
-    if (files.length) {
-      try {
-        const filecontent = fs.readFileSync(files[0]).toString();
-      } catch (error) {
-        console.error(`Unable to read file ${files[0]}`);
-        throw error;
-      }
-      glob(
-        `node_modules/markdown-magic-branch-badge/**/post-checkout`,
-        function(er, files) {
-          if (er) {
-            console.error("Unable to fetch githook.");
-            throw er;
-          }
+    glob(".git/hooks/post-checkout", function(err, files) {
+      if (err) {
+        log(chalk`{bold.redBright Error: }{yellowBright \n${err}}`);
+      } else {
+        if (files[0]) {
+          // A githook already exists in repository.
           try {
-            const hookcontent = fs.readFileSync(
-              path.join(process.cwd(), files[0])
+            glob(
+              `node_modules/markdown-magic-branch-badge/**/post-checkout`,
+              function(er, files) {
+                if (er) {
+                  log(
+                    chalk`{bold.redBright Error fetching githook: }{yellowBright \n${er}}`
+                  );
+                }
+                try {
+                  const hookcontent = fs.readFileSync(
+                    path.join(process.cwd(), files[0])
+                  );
+                  const filecontent = fs.readFileSync(files[0]).toString();
+                  if (filecontent.indexOf(hookcontent) >= 0) {
+                    log(
+                      chalk.keyword("orange")(
+                        "Post-checkout hook already exists!"
+                      )
+                    );
+                    return;
+                  } else {
+                    // add hook to original hook
+                  }
+                } catch (error) {
+                  log(
+                    chalk`{bold.redBright Error reading githook: }{yellowBright \n${error}}`
+                  );
+                }
+              }
             );
-            const filecontent = fs.readFileSync(files[0]).toString();
-            if (filecontent.indexOf(hookcontent) >= 0) {
-              console.log("post-checkout hook already exists");
-            }
           } catch (error) {
-            console.error("Unable to fetch githook.");
-            throw error;
+            throw err;
           }
+        } else {
+          // Add githook to the git repository.
+          glob(
+            `node_modules/markdown-magic-branch-badge/**/post-checkout`,
+            function(er, files) {
+              if (er) {
+                log(
+                  chalk`{bold.redBright Error fetching githook: }{yellowBright \n${er}}`
+                );
+              }
+              shell.cp(files[0], path.join(process.cwd(), ".git/hooks/"));
+              log(chalk.greenBright`Successfully added githook.`);
+            }
+          );
         }
-      );
-    } else {
-      glob(
-        `node_modules/markdown-magic-branch-badge/**/post-checkout`,
-        function(er, files) {
-          if (er) {
-            console.error("Unable to fetch githook.");
-            throw er;
-          }
-          shell.cp(files[0], path.join(process.cwd(), ".git/hooks/"));
-          log(chalk.greenBright`Successfully added githook.`);
-        }
-      );
-    }
+      }
+    });
   });
 };
 
 const updateFile = path => {
   if (!path) {
     log(
-      chalk`{bold.redBright ${`error: `}}{yellowBright ${`Please specify the path to the file.`}}\n{greenBright ${`For details on how to use, please refer docs: `}}{underline.green ${"https://github.com/rishichawda/markdown-magic-build-badge"}}`
+      chalk`{bold.redBright Error: }{yellowBright Please specify the path to the file.}\n{greenBright For details on how to use, please refer docs: }{underline.green https://github.com/rishichawda/markdown-magic-build-badge}`
     );
   } else {
     glob(path, function(err, files) {
@@ -75,11 +91,11 @@ const updateFile = path => {
           shell.exec(`node ${files[0]}`);
         } else if (files.length > 1) {
           log(
-            chalk`{keyword('orange') ${"warning: "}}{yellowBright ${"Multiple files with the same name found! Exiting.."}}\n{bold.greenBright ${"Please specify the path to the plugin's entry file."}}`
+            chalk`{keyword('orange') Warning: }{yellowBright Multiple files with the same name found! Exiting..}\n{bold.greenBright Please specify the path to the plugin's entry file.}`
           );
         } else {
           log(
-            chalk`{bold.redBright ${`error: `}}{yellowBright ${`Failed to find the file specified.`}}`
+            chalk`{bold.redBright Error: }{yellowBright Failed to find the file specified.}`
           );
         }
       }
