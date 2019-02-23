d7a6dfdfd4eeae2419857926c4a3d5437a2972a0
rishichawda <chawda.rishii@gmail.com>
2019-02-23 17:54:38 +0530

    fix(bin/mdb): append on existing hook

--- bin/mdb.js
+++ bin/mdb.js
@@ -7,7 +7,7 @@ const log = console.log;
 
 const generateHook = () => {
   glob(".git", function(err, files) {
-    if (err || !files.length) {
+    if (err || !files.length || !shell.which("git")) {
       log(
         chalk`{bold.redBright Error: }{yellowBright ${err ||
           `Unable to verify as a git repository. Try running \`git init\` and initialising`}} ${chalk.black.bgYellowBright(
@@ -25,16 +25,17 @@ const generateHook = () => {
           try {
             glob(
               `node_modules/markdown-magic-branch-badge/**/post-checkout`,
-              function(er, files) {
+              function(er, file) {
                 if (er) {
                   log(
                     chalk`{bold.redBright Error fetching githook: }{yellowBright \n${er}}`
                   );
+                  return;
                 }
                 try {
-                  const hookcontent = fs.readFileSync(
-                    path.join(process.cwd(), files[0])
-                  );
+                  const hookcontent = fs
+                    .readFileSync(path.join(process.cwd(), file[0]))
+                    .toString();
                   const filecontent = fs.readFileSync(files[0]).toString();
                   if (filecontent.indexOf(hookcontent) >= 0) {
                     log(
@@ -45,11 +46,25 @@ const generateHook = () => {
                     return;
                   } else {
                     // add hook to original hook
+                    if (filecontent.indexOf("#!/bin/bash") >= 0) {
+                      hookcontent = hookcontent.split("\n").slice(2).join("\n");
+                    }
+                    fs.appendFile(files[0], `\n${hookcontent}`, function(err) {
+                      if (err) {
+                        log(
+                          chalk`{bold.redBright Error adding githook: }{yellowBright \n${err}}`
+                        );
+                      } else {
+                        log(chalk.greenBright`Successfully added githook.`);
+                      }
+                    });
+                    return;
                   }
                 } catch (error) {
                   log(
                     chalk`{bold.redBright Error reading githook: }{yellowBright \n${error}}`
                   );
+                  return;
                 }
               }
             );
